0922f53d61583c4d631324dd054005a3a6679d7e
Doug SImpson <doug.simpson@adasphere.com>
2018-05-23 14:09:00 -0400

    Stub outs tests for the Models

---
+++ AER.Enigma.Core.Tests/CoreExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AER.Enigma.Core.Tests
+{
+    [TestClass]
+    public class CoreExtensionsTests
+    {
+        [TestMethod]
+        public void ToNullableDoubleTest()
+        {
+            Assert.Fail("Test not implemented");
+        }
+
+        [TestMethod]
+        public void ToNullableIntTest()
+        {
+            Assert.Fail("Test not implemented");
+        }
+    }
+}
---
+++ AER.Enigma.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AER.Enigma.Services.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AER.Enigma.Services.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("413c78b4-57b5-4554-a897-b30642798d92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
